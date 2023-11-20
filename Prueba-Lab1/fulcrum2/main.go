41b6f824c59fdfd8e4040e931eee8c1984d45bd2
Nicolas <nicolas.pizarro@alumnos.inf.utfsm.cl>
2023-11-20 16:55:57 -0300

    prueba

--- Prueba-Lab1/fulcrum2/main.go
+++ Prueba-Lab1/fulcrum2/main.go
@@ -19,6 +19,45 @@ type server struct {
 }
 
 func (s *server) Create(ctx context.Context, req *pb.Crearmensaje) (*pb.Respuestamensaje, error) {
+	//Extra
+	conn1, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		panic("no se puede conectar con el servidor 1" + err.Error())
+	}
+	defer conn1.Close()
+
+	serviceClient1 := pb.NewMensajeServiceClient(conn1)
+
+	_, err := serviceClient1.CreateActualiza(context.Background(), &pb.CrearActualizacion{
+		Actualiza: &pb.Actualizar{
+			Nombre: req.Mensaje.Nombre,
+		},
+	})
+
+	if err != nil {
+		panic("no se creo el mensaje en el servidor 1" + err.Error())
+	}
+
+	conn2, err := grpc.Dial("localhost:50054", grpc.WithInsecure())
+	if err != nil {
+		panic("no se puede conectar con el servidor 2" + err.Error())
+	}
+	defer conn2.Close()
+
+	serviceClient2 := pb.NewMensajeServiceClient(conn2)
+
+	_, err := serviceClient2.CreateActualiza(context.Background(), &pb.CrearActualizacion{
+		Actualiza: &pb.Actualizar{
+			Nombre: req.Mensaje.Nombre,
+		},
+	})
+
+	if err != nil {
+		panic("no se creo el mensaje en el servidor 2" + err.Error())
+	}
+
+	//Importante
+
 	fmt.Println("Solicitud de " + req.Mensaje.Nombre + " recibida, mensaje enviado: " + req.Mensaje.Nombre)
 
 	palabras := strings.Split(req.Mensaje.Nombre, " ")
@@ -232,6 +271,160 @@ func (s *server) CreateLista(ctx context.Context, req *pb.ConsultarLista) (*pb.R
 		Estadoid: lista,
 	}, nil
 
+}
+
+func (s *server) CreateActualiza(ctx context.Context, req *pb.CrearActualizacion) (*pb.RespuestaActualizacion, error) {
+	fmt.Println("Solicitud de " + req.Actualiza.Nombre + " recibida, mensaje enviado: " + req.Actualiza.Nombre)
+
+	palabras := strings.Split(req.Actualiza.Nombre, " ")
+
+	if palabras[0] == "AgregarBase" {
+		archivo, err := os.OpenFile(palabras[1]+".txt", os.O_RDWR|os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("Error al abrir el archivo", err)
+			return nil, err
+		}
+
+		defer archivo.Close()
+
+		_, err = archivo.WriteString(palabras[2] + " " + palabras[3] + "\n")
+
+		if err != nil {
+			fmt.Println("Error al escribir en el archivo", err)
+			return nil, err
+		}
+
+	} else if palabras[0] == "RenombrarBase" {
+		archivo, err := os.OpenFile(palabras[1]+".txt", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("Error al abrir el archivo", err)
+			return nil, err
+		}
+
+		defer archivo.Close()
+
+		var nuevoContenido []string
+		scanner := bufio.NewScanner(archivo)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			div := strings.Fields(line)
+
+			if div[0] == palabras[2] {
+				div[0] = palabras[3]
+				line = strings.Join(div, " ")
+			}
+
+			nuevoContenido = append(nuevoContenido, line)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error al escanear el archivo:", err)
+			return nil, err
+		}
+
+		if _, err := archivo.Seek(0, 0); err != nil {
+			fmt.Println("Error al rebobinar el archivo:", err)
+			return nil, err
+		}
+
+		if err := archivo.Truncate(0); err != nil {
+			fmt.Println("Error al truncar el archivo:", err)
+			return nil, err
+		}
+
+		for _, linea := range nuevoContenido {
+			fmt.Fprintln(archivo, linea)
+		}
+
+	} else if palabras[0] == "ActualizarValor" {
+		archivo, err := os.OpenFile(palabras[1]+".txt", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("Error al abrir el archivo", err)
+			return nil, err
+		}
+
+		defer archivo.Close()
+
+		var nuevoContenido []string
+		scanner := bufio.NewScanner(archivo)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			div := strings.Fields(line)
+
+			if div[0] == palabras[2] {
+				div[1] = palabras[3]
+				line = strings.Join(div, " ")
+			}
+
+			nuevoContenido = append(nuevoContenido, line)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error al escanear el archivo:", err)
+			return nil, err
+		}
+
+		if _, err := archivo.Seek(0, 0); err != nil {
+			fmt.Println("Error al rebobinar el archivo:", err)
+			return nil, err
+		}
+
+		if err := archivo.Truncate(0); err != nil {
+			fmt.Println("Error al truncar el archivo:", err)
+			return nil, err
+		}
+
+		for _, linea := range nuevoContenido {
+			fmt.Fprintln(archivo, linea)
+		}
+
+	} else if palabras[0] == "BorrarBase" {
+		archivo, err := os.OpenFile(palabras[1]+".txt", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("Error al abrir el archivo", err)
+			return nil, err
+		}
+
+		defer archivo.Close()
+
+		var nuevoContenido []string
+		scanner := bufio.NewScanner(archivo)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			div := strings.Fields(line)
+
+			if div[0] != palabras[2] {
+				nuevoContenido = append(nuevoContenido, line)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error al escanear el archivo:", err)
+			return nil, err
+		}
+
+		if err := archivo.Truncate(0); err != nil {
+			fmt.Println("Error al truncar el archivo:", err)
+			return nil, err
+		}
+
+		if _, err := archivo.Seek(0, 0); err != nil {
+			fmt.Println("Error al rebobinar el archivo:", err)
+			return nil, err
+		}
+
+		for _, linea := range nuevoContenido {
+			fmt.Fprintln(archivo, linea)
+		}
+
+	}
+
+	return &pb.RespuestaActualizacion{
+		Actualizaid: req.Actualiza.Nombre,
+	}, nil
 }
 
 func consistenciaEventual() {
